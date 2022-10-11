13322e02d847de810372720fad9eba46587a5bef
Kian0607 <113333320+Kian0607@users.noreply.github.com>
2022-10-11 12:40:14 +0800

    Create main.py

---
+++ activity 5/main.py
@@ -0,0 +1,18 @@
+import sqlite3
+from flask import Flask, render_template
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory= sqlite3.Row
+    return conn
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    conn= get_db_connection()
+    posts= conn.execute( 'SELECT * FROM post').fetchall()
+    conn.close()
+    return render_template('index.html', post=posts)
+
+app.run(host= "localhost", debug=True)
